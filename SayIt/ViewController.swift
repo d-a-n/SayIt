b50d2ee546697eac2ff5c9bd5b839362c4e7b7cc
dan <dan@imake.io>
2017-08-13 00:16:23 +0200

    Initial Commit

---
+++ SayIt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SayIt
+//
+//  Created by dan on 8/13/17.
+//  Copyright © 2017 dan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
