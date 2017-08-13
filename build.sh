bb8a9c19526d19e93083d59544b0575071207a28
dan <dan@imake.io>
2017-08-13 21:03:35 +0200

    extend build script

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 cd spm
 swift package fetch
 swift package update
+swift package generate-xcodeproj
 cd ..
 xcodebuild clean && xcodebuild build -project SayIt.xcodeproj  -scheme SayIt-Release CODE_SIGNING_REQUIRED=NO -derivedDataPath ./build
